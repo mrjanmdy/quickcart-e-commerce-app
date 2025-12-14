9d6fc678d12a4c7193c01b29af9e45c4c7a011d2
maryjane <maryjjane.mdy2003@gmail.com>
2025-12-14 19:02:08 +0330

    save

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -1,6 +1,6 @@
 import { inngest } from "@/config/inngest";
 import Product from "@/models/Product";
-import User from "@/models/User";
+import User from "@/models/user";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
--- app/api/user/data/route.js
+++ app/api/user/data/route.js
@@ -1,5 +1,5 @@
 import connectDB from "@/config/db";
-import User from "@/models/User";
+import User from "@/models/user";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
